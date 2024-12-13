ba9512ff7c37601df39f60d902deef237927605c
Pofig25 <barmindm@gmail.com>
2024-12-13 19:58:22 +1000

    Update module_6_2.py

--- module_6_2.py
+++ module_6_2.py
@@ -2,37 +2,37 @@
 # а Sedan(седан) - наследник класса Vehicle.
 
 class Vehicle:
-    __COLOR_VARIANTS = ['green', 'red', 'grey', 'white', 'brown', 'black']  # Атрибут класса __COLOR_VARIANTS (Цвета свои)
+    __COLOR_VARIANTS = ['green', 'red', 'grey', 'white', 'brown', 'black']
 
     def __init__(self, owner, __model, __color, __engine_power):
-        self.owner = owner                                         # owner(str) - владелец транспорта. (может меняться)
-        self.__model = __model                   # __model(str) - модель (марка) транспорта. (не менять название модели)
-        self.__color = __color                   # __color(str) - название цвета. (не менять цвет автомобиля)
-        self.__engine_power = __engine_power  # __engine_power(int) - мощность двигателя. (не менять мощность двигателя)
+        self.owner = owner
+        self.__model = __model
+        self.__color = __color
+        self.__engine_power = __engine_power
 
-    def get_model(self):                         # get_model - возвращает строку: "Модель: <название модели транспорта>"
+    def get_model(self):
         print(f'Модель: {self.__model}')
 
-    def get_horsepower(self):                    # get_horsepower - возвращает строку: "Мощность двигателя: <мощность>"
+    def get_horsepower(self):
         print(f'Мощность двигателя: {self.__engine_power}')
 
-    def get_color(self):                         # get_color - возвращает строку: "Цвет: <цвет транспорта>"
+    def get_color(self):
         print(f'Цвет: {self.__color}')
 
-    def print_info(self):                        # print_info - распечатывает результаты методов (в том же порядке)
+    def print_info(self):
         Vehicle.get_model(self)
         Vehicle.get_horsepower(self)
         Vehicle.get_color(self)
-        print(f'Владелец: {self.owner}')         # а так же владельца в конце в формате "Владелец: <имя>"
-
-    def set_color(self, new_color):              # Метод set_color - принимает аргумент new_color(str), меняет цвет __color на new_color,
-        if new_color.lower() in self.__COLOR_VARIANTS:     # если он есть в списке __COLOR_VARIANTS,
+        print(f'Владелец: {self.owner}')
+        
+    def set_color(self, new_color):
+        if new_color.lower() in self.__COLOR_VARIANTS:
             self.__color = new_color
-        else:                                              # в противном случае
-            print(f'Нельзя поменять цвет на {new_color}')  # выводит на экран надпись: "Нельзя сменить цвет на <новый цвет>".
+        else:
+            print(f'Нельзя поменять цвет на {new_color}')
 
 class Sedan(Vehicle):
-    __PASSENGERS_LIMIT = 5                       # __PASSENGERS_LIMIT = 5 (в седан может поместиться только 5 пассажиров)
+    __PASSENGERS_LIMIT = 5
 
 # Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
